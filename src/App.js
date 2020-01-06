504d59f3cb58e7d825be3a9e60731415cde70735
hahamen-ihs <todayspirit@gmail.com>
2020-01-06 16:16:06 +0700

    membuat file login.html

--- src/App.js
+++ src/App.js
@@ -2,25 +2,57 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends React.Component {
+  state = {
+    data : "",
+    umur : 0
+  }
+
+  changeHandler = (event) => {
+    const {value,name} = event.target
+    console.log(name)
+    this.setState({
+      [name] : value
+    })
+  }
+ 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+    .then(res => this.setState({
+      data: res
+    }) )
+  }
+
+  /* jadi this.setState untuk merubah nilai state diatas */
+  
+
+  render(){
+    const {data} = this.state
+    return(
+        <div className="App"> 
+        <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Ihsandroid</h1>
+        {/* ternary operator,map, constructor,ambil api yg ? dan :-->*/}
+        {data ? 
+          data.map(datum => {
+            const {title,userId} = datum 
+            return <p key={userId + title}>
+                    {title} {userId}
+                    </p>
+          })
+          : <h1>Loading....</h1>
+        }
+{/* karena pakai .map maka di <p> nya harus ada key dari yang kita ambil disini
+  adalah title dan userId
+  */}
+        
       </header>
     </div>
-  );
+
+    )
+  }
 }
 
 export default App;

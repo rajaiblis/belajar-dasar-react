53268edaebf094ef5967067ee7c157a805dd14a7
hahamen-ihs <todayspirit@gmail.com>
2020-01-07 10:20:12 +0700

    propsstate

--- src/App.js
+++ src/App.js
@@ -11,16 +11,36 @@ class App extends React.Component {
     tempatTinggal : "Tangsel",
     umur: 24,
     laptop : "acer",
-    merk : "ori"
+    merk : "ori",
+    data : "",
+    angka : 0
   }
+
+changeHandler = (event) => {
+  this.setState({
+    [event.target.name]: event.target.value
+  })
+}
+
   render(){
     return(
         <div className="App"> 
         <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Ihsandroid</h1>
-        <ComponentKu 
-        {... this.state}
+        <input 
+          type="text"
+          value={this.state.data}
+          onChange={this.changeHandler}
+          name="data"
+        />
+        <input 
+          type="number"
+          value={this.state.angka}
+          onChange={this.changeHandler}
+          name="angka"
+        />
+        <ComponentKu hasilKu={this.state.data} angkaKu={this.state.angka}
         />
       </header>
     </div>
@@ -29,9 +49,21 @@ class App extends React.Component {
 }
 
 export default App;
+
 /**
- * {... this.state} berguna untuk jika kita punya banyak data di dalam object state
- * dan cara ini dinamakan rest and spread props atau penggabungan sehingga mau ada
- * 100 data di state akan mudah dimasukkan dan enak dibaca cukup menambahkan ... 
- * sebelum kode kita
+ * disini kita buat arrow func dengan args event karena ini untuk merubahnya
+ * dan menggunakan this.setState lalu di dalam () kita kasih func berbentuk object {}
+ * maka ketika kita ketik dalam kolom input maka akan merubah data hasilKu dan memanggil
+ * props hasilKu di componentKu.js
+ * di input kedua kita ganti jadi angka dan di state kita kasih angka dan mulai dari 0
+ * sehingga kita merubah this.state.angka dan name=angka
+ * dan tambahkan angkaKu={this.state.angkaKu} di dalam JSX componentKu
+ * onchange masih sama karena kita memakai 1 func untuk semua nya.
+ * this.setState({
+    data : event.target.value ini untuk data biasa, kalau untuk lebih dari 1 kita buat jadi array
+    contoh :
+    this.setState({
+    [event.target.name]: event.target.value
+    ingat harus sesuai this.state dengan state yang ada contoh {this.state.angka} berarti memanggil state angka
+    karena kita sudah buat state angka yang tadi
  */
--- src/componentKu.js
+++ src/componentKu.js
@@ -1,26 +1,13 @@
 import React from 'react'
 
 const componentKu = (props) => {
-    const {pekerjaan,tempatTinggal,umur,laptop,merk} = props
-    return <p>ini adalah component saya dan {pekerjaan} {tempatTinggal} {umur} {laptop} {merk} </p>
+    const {hasilKu, angkaKu} = props
+    return <p> ini adalah {hasilKu} ada {angkaKu} Rupiah </p>
 }
 
 export default componentKu
 
-/* 
-* const componentKu = () => {
-    return <p>ini componentKu</p>
-}
-export default componentKu
-cara pertama dasar untuk props
-
-* cara kedua kita tambahankan argumen props di const componentku lalu tambahkan
-{} karena file nya JSX lalu tambahkan props yang di app.js sehingga menjadi
-{props.pekerjaan} 
-
-atau dengan desctruturing const {pekerjaan} = props
-component bisa diisi dengan banyak props
-contoh saya tambahkan tempatTinggal & umur lalu tambah di desctruturing
-
-
-export default componentKu agar component nya bisa di export*/
+/**
+ * disini yang kita parsing adalah property JSX dari hasilKu di app.js jadi
+ * destrucring nya kita isi hasilKu dan angkaKu dan memanggil keduanya
+ */
